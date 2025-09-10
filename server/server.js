02e00cbdd2e8a84b87204030b566be4fbe18994b
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-09-10 21:51:13 +0530

    feat: build authController authRoute and register, login and logout api

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,8 +15,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+// API End point
 app.get('/', (req, res) => {
   res.send('Hello Satyam from Express!');
 });
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
