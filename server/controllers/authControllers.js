166d1062ebb6afbd8a2de32078b4dacdfa3c77d1
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-09-11 00:05:55 +0530

    feat: build funcnality of nodemailer and get otp by mail and verify email through otp

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodeMailer.js';
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -28,6 +29,16 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
+
+        // Sending welcome email
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'Welcome to Our App!',
+            text: `Welcome to our website. your account has been created successfully with email id: ${email}.`
+        }
+
+        await transporter.sendMail(mailOptions);
 
         return res.json({success: true, message: "User created succesfully!!"});
 
@@ -83,6 +94,79 @@ export const logout = async (req, res) => {
         });
 
         return res.json({success: true, message: "Logout successful!!"});
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
+
+
+// send Verification OTP to the user's Email
+export const sendVerifyOtp = async (req, res) => {
+    try {
+
+        const {userId} = req.body;
+
+        const user = await userModel.findById(userId);
+        if(!user) {
+            return res.json({success: false, message: "User not found"});
+        }
+
+        if(user.isAccountVerified) {
+            return res.json({success: false, message: "Account already verified"});
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000; // 24 hours
+
+        await user.save();
+
+        const mailOption = {
+             from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: 'Verify Your Email',
+            text: `Your verification OTP is: ${otp}. It is valid for 24 hours.`
+        }
+
+        await transporter.sendMail(mailOption);
+
+        return res.json({success: true, message: "OTP sent to your email"});
+
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
+
+export const verifyEmail = async (req, res) => {
+    const {userId, otp} = req.body;
+
+    if(!userId || !otp) {
+        return res.json({success: false, message: "All fields are required"});
+    }
+
+    try {
+
+        const user = await userModel.findById(userId);
+        if(!user) {
+            return res.json({success: false, message: "User not found"});
+        }
+
+        if(user.verifyOtp === '' || user.verifyOtp !== otp){
+            return res.json({success: false, message: 'Invalid OTP'});
+        }
+
+        if(user.verifyOtpExpireAt < Date.now()){
+            return res.json({success: flase, message: 'OTP Expire'});
+        } 
+
+        user.isAccountVerified = true;
+        user.verifyOtp = '';
+        user.verifyOtpExpireAt = 0;
+
+        await user.save();
+        return res.json({success: true, message: 'Email verified successfully'});
+        
     } catch (error) {
         return res.json({success: false, message: error.message});
     }
