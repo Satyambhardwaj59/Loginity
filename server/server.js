3cd79f6e6d41ce7bb740de3b396ce389434b94b0
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-09-11 12:10:19 +0530

    Feat: build get user data api , and complete backend

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(cors({credentials: true}));
 app.get('/', (req, res) => {
   res.send('Hello Satyam from Express!');
 });
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter)
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
